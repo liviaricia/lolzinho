b87414f037a43a75aff7d1b5da9efff4d7aecc0d
LiviAricia <livia_aricia@hotmail.com>
2021-11-24 21:48:27 -0300

    Inicialização masterpage website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MasterPageModule } from './master-page';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MasterPageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
